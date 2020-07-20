04f48f0fe28a863f3c2c05ff323eddce5b07210a
Karl Meakin <karlwfmeakin@gmail.com>
2020-07-20 14:51:57 +0100

    afdasdf

--- crates/simpl-types2/src/ast.rs
+++ crates/simpl-types2/src/ast.rs
@@ -1,32 +1,146 @@
+use crate::{
+    ty::{Type, TypeEnv},
+    FromId, IdGen,
+};
+use simpl_syntax2::ast::Expr;
 pub use simpl_syntax2::ast::{Ident, Lit};
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum TypedExpr {
     Lit {
+        ty: Type,
         val: Lit,
     },
     Var {
+        ty: Type,
         name: String,
     },
     If {
+        ty: Type,
         test: Box<Self>,
         then_branch: Box<Self>,
         else_branch: Box<Self>,
     },
     Let {
+        ty: Type,
         bindings: Vec<(Ident, Self)>,
         body: Box<Self>,
     },
     Letrec {
+        ty: Type,
         bindings: Vec<(Ident, Self)>,
         body: Box<Self>,
     },
     Lambda {
+        ty: Type,
         params: Vec<Ident>,
         body: Box<Self>,
     },
     App {
+        ty: Type,
         func: Box<Self>,
         arg: Box<Self>,
     },
+}
+
+type TypeVarGen = IdGen<Type>;
+impl FromId for Type {
+    fn from_id(id: u32) -> Type {
+        Type::Var(id)
+    }
+}
+
+impl TypedExpr {
+    pub fn from_ast(ast: Expr) -> Result<Self, String> {
+        let mut gen = TypeVarGen::new();
+        let tenv = TypeEnv::default();
+        Self::from_ast_inner(ast, &tenv, &mut gen)
+    }
+
+    fn from_ast_inner(ast: Expr, tenv: &TypeEnv, gen: &mut TypeVarGen) -> Result<Self, String> {
+        let expr = match ast {
+            Expr::Lit { val } => Self::Lit {
+                val,
+                ty: gen.next(),
+            },
+            Expr::Var { name } => match tenv.get(&name) {
+                None => return Err(format!("Unbound variable: {}", name)),
+                Some(ty) => Self::Var {
+                    ty: ty.clone(),
+                    name,
+                },
+            },
+            Expr::If {
+                test,
+                then_branch,
+                else_branch,
+            } => Self::If {
+                ty: gen.next(),
+                test: box Self::from_ast_inner(*test, tenv, gen)?,
+                then_branch: box Self::from_ast_inner(*then_branch, tenv, gen)?,
+                else_branch: box Self::from_ast_inner(*else_branch, tenv, gen)?,
+            },
+            Expr::Let { bindings, body } => {
+                let ty = gen.next();
+                let mut extended_tenv = tenv.clone();
+                let mut new_bindings = Vec::new();
+
+                for (name, expr) in bindings {
+                    // Make sure you extend the tenv *after* annotating the value, so that circular
+                    // references raise an "unbound variable" error
+
+                    let tvar = gen.next();
+                    let typed_expr = Self::from_ast_inner(expr, &extended_tenv, gen)?;
+                    new_bindings.push((name.clone(), typed_expr));
+                    extended_tenv.insert(name, tvar.clone());
+                }
+
+                Self::Let {
+                    ty,
+                    bindings: new_bindings,
+                    body: box Self::from_ast_inner(*body, &extended_tenv, gen)?,
+                }
+            }
+            Expr::Letrec { bindings, body } => {
+                let ty = gen.next();
+                let mut extended_tenv = tenv.clone();
+                for (name, _) in &bindings {
+                    extended_tenv.insert(name.clone(), gen.next());
+                }
+
+                let mut new_bindings = Vec::new();
+
+                for (name, expr) in bindings {
+                    let typed_expr = Self::from_ast_inner(expr, &extended_tenv, gen)?;
+                    new_bindings.push((name, typed_expr));
+                }
+
+                Self::Letrec {
+                    ty,
+                    bindings: new_bindings,
+                    body: box Self::from_ast_inner(*body, &extended_tenv, gen)?,
+                }
+            }
+            Expr::Lambda { params, body } => {
+                let ty = gen.next();
+                let mut extended_tenv = tenv.clone();
+
+                for name in &params {
+                    extended_tenv.insert(name.clone(), gen.next());
+                }
+
+                Self::Lambda {
+                    ty,
+                    params,
+                    body: box Self::from_ast_inner(*body, &extended_tenv, gen)?,
+                }
+            }
+            Expr::App { func, arg } => Self::App {
+                ty: gen.next(),
+                func: box Self::from_ast_inner(*func, tenv, gen)?,
+                arg: box Self::from_ast_inner(*arg, tenv, gen)?,
+            },
+        };
+        Ok(expr)
+    }
 }
