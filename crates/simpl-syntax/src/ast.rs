ea9ca2fd128e45f761c1e40a36ee841948dfc364
Karl Meakin <karlwfmeakin@gmail.com>
2020-07-21 02:14:02 +0100

    add some helpful trait defs for Lit

--- crates/simpl-syntax/src/ast.rs
+++ crates/simpl-syntax/src/ast.rs
@@ -1,4 +1,5 @@
 pub type Ident = String;
+use derive_more::Display;
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum Expr {
@@ -31,9 +32,30 @@ pub enum Expr {
     },
 }
 
-#[derive(Debug, Clone, PartialEq)]
+#[derive(Debug, Clone, PartialEq, Display)]
 pub enum Lit {
+    #[display(fmt = "{}", _0)]
     Bool(bool),
+    #[display(fmt = "{}", _0)]
     Int(i64),
+    #[display(fmt = "{}", _0)]
     Float(f64),
+}
+
+impl From<bool> for Lit {
+    fn from(other: bool) -> Self {
+        Self::Bool(other)
+    }
+}
+
+impl From<i64> for Lit {
+    fn from(other: i64) -> Self {
+        Self::Int(other)
+    }
+}
+
+impl From<f64> for Lit {
+    fn from(other: f64) -> Self {
+        Self::Float(other)
+    }
 }
