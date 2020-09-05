8f2029005191c6eca0ffe495cb75b50385be476c
Karl Meakin <karlwfmeakin@gmail.com>
2020-09-05 14:07:34 +0100

    delete unused c codegen backend

--- src/codegen-c/mod.rs
+++
@@ -1,6 +0,0 @@
-mod anf;
-mod closure;
-mod compile;
-mod gensym;
-mod rename;
-mod util;
--- src/codegen-c/prelude.h
+++
@@ -1,7 +0,0 @@
-#include <stdbool.h>
-#include <stdint.h>
-#include <stdio.h>
-
-typedef int64_t Int;
-typedef bool Bool;
-typedef double Float;
