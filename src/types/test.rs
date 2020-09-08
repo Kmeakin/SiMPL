29e2fd5f4985267667369b466f9cb04d67b2e805
Karl Meakin <karlwfmeakin@gmail.com>
2020-09-08 16:04:11 +0100

    refactor type inference tests

--- src/types/test.rs
+++ src/types/test.rs
@@ -14,43 +14,47 @@ fn test_infer(src: &str, expected: Type) {
 }
 
 #[test]
+fn infer_lit() {
+    test_infer("1", Int);
+    test_infer("1.0", Float);
+    test_infer("true", Bool);
+    test_infer("false", Bool);
+}
+
+#[test]
 fn infer_identity_fn() {
-    let expr = Expr::from_str(r"\x -> x").unwrap();
-    let ty = type_of(&expr);
-    assert_eq!(ty, ty![{1} => {1}]);
+    test_infer(r"\x -> x", ty![{1} => {1}]);
 }
 
 #[test]
 fn infer_const_fn() {
-    let expr = Expr::from_str(r"\a -> \b -> a").unwrap();
-    let ty = type_of(&expr);
-    assert_eq!(ty, ty![{1} => {3} => {1}])
+    test_infer(r"\a -> \b -> a", ty![{1} => {3} => {1}]);
 }
 
 #[test]
 fn infer_compose_fn() {
-    let expr = Expr::from_str(r"\f -> \g -> \x -> f (g x)").unwrap();
-    let ty = type_of(&expr);
-    assert_eq!(ty, ty![({8} => {6}) => ({5} => {8}) => {5} => {6}]);
+    test_infer(
+        r"\f -> \g -> \x -> f (g x)",
+        ty![({8} => {6}) => ({5} => {8}) => {5} => {6}],
+    );
 }
 
 #[test]
 fn infer_pred_fn() {
-    let expr = Expr::from_str(r"\pred -> if pred 1 then 2 else 3").unwrap();
-    let ty = type_of(&expr);
-    assert_eq!(ty, ty![(Int => Bool) => Int]);
+    test_infer(
+        r"\pred -> if pred 1 then 2 else 3",
+        ty![(Int => Bool) => Int],
+    );
 }
 
 #[test]
 fn infer_inc_fn() {
-    let expr = Expr::from_str(r"let inc = \x -> x + 1 in inc (inc 1)").unwrap();
-    let ty = type_of(&expr);
-    assert_eq!(ty, ty![Int])
+    test_infer(r"let inc = \x -> x + 1 in inc (inc 1)", ty![Int]);
 }
 
 #[test]
 fn infer_letrec() {
-    let expr = Expr::from_str(
+    test_infer(
         r"
 letrec
     countdown = \x -> if is_zero x
@@ -58,15 +62,13 @@ letrec
                       else countdown (sub x 1)
 in
     countdown",
-    )
-    .unwrap();
-    let ty = type_of(&expr);
-    assert_eq!(ty, ty![Int => Int])
+        ty![Int => Int],
+    );
 }
 
 #[test]
 fn infer_letrec_mutually_recursive() {
-    let expr = Expr::from_str(
+    test_infer(
         r"
 letrec
     is_even = \x -> if is_zero x
@@ -77,65 +79,29 @@ letrec
                       else is_even (sub x 1)
 in
     is_even",
-    )
-    .unwrap();
-    let ty = type_of(&expr);
-    assert_eq!(ty, ty![Int => Bool])
+        ty![Int => Bool],
+    );
 }
 
 #[test]
 fn infer_let_many() {
-    let expr = Expr::from_str(
-        r"
-let
-    x = 1,
-    y = add x 1,
-    z = \a -> add a y
-in
-    z",
-    )
-    .unwrap();
-    let ty = type_of(&expr);
-    assert_eq!(ty, ty![Int => Int])
+    test_infer(
+        r"let x = 1, y = add x 1, z = \a -> add a y in z",
+        ty![Int => Int],
+    );
 }
 
 #[test]
 fn infer_lambda_many() {
-    let expr = Expr::from_str(
-        r"
-let
-    f = \a, b -> a b
-in
-    f",
-    )
-    .unwrap();
-    let ty = type_of(&expr);
-    assert_eq!(ty, ty![({5} => {6}) => {5} => {6}])
+    test_infer(
+        r"let f = \a, b -> a b in f",
+        ty![({5} => {6}) => {5} => {6}],
+    );
 }
 
 #[test]
 fn infer_annotations() {
-    let expr = Expr::from_str(
-        r"
-let
-    idInt = \a: Int -> a
-in
-    idInt",
-    )
-    .unwrap();
-    let ty = type_of(&expr);
-    assert_eq!(ty, ty![Int => Int]);
-
-    let expr = Expr::from_str(
-        r"
-let
-    idInt: Int -> Int = \a -> a
-in
-    idInt",
-    )
-    .unwrap();
-    let ty = type_of(&expr);
-    assert_eq!(ty, ty![Int => Int])
+    test_infer(r"let idInt: Int -> Int = \a -> a in idInt", ty![Int => Int]);
 }
 
 #[test]
